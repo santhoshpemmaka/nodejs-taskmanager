664f1abc437096414415942e1def39bcc6bbdc51
santoshpemmaka <113825050+santoshpemmaka@users.noreply.github.com>
2023-08-30 17:45:51 +0530

    update app.js

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const databaseUrl = process.env.DATABASE_URL;
 app.use(bodyParsed.json());
 app.use('/task-manager',taskRoutes);
 mongoose.set("strictQuery", false);
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','*');
+    res.setHeader("Access-Control-Allow-Headers",'Content-Type, Authorization');
+    req.next();
+});
 mongoose.connect(databaseUrl)
 .then(result => {
     const server = app.listen(8080);
